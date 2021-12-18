bc1229aa81be41c935f51a91321b640a1e054e77
linuszlx <linuszhang@126.com>
2021-12-18 12:57:57 +0800

    Update Flyt.js

--- JS/Flyt.js
+++ JS/Flyt.js
@@ -1,4 +1,4 @@
 let body = $response.body;
-let obj = JSON.parse(body);
-obj = obj.replace('adv', 'abc');
+var obj = JSON.parse(body);
+obj = obj.replace(/adv/, /abc/);
 $done({body: JSON.stringify(obj)});
