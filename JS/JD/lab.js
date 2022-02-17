cf57bc9b4d5bf8d1a2972eb4eeb5eda70313b490
linuszlx <linuszhang@126.com>
2022-02-17 13:05:46 +0800

    Create lab.js

---
+++ JS/JD/lab.js
@@ -0,0 +1,79 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.result.normal= {
+        "navigationAll" : [
+          {
+            "lableName" : "首页",
+            "functionId" : "index",
+            "url" : "openappurl:dsaflkkjldasf",
+            "navigationId" : 0,
+            "lableImageMd5" : "ed1a9a8bd671468b0e2cc45bccecc4f9",
+            "bigIconIsOpen" : "0",
+            "tabNameSelected" : "首页",
+            "optlabelColor" : "#E2231A",
+            "useType" : -1,
+            "optlableImageMd5" : "843b4a7e9a5232557a96d41b2564f71d",
+            "clickEventId" : "Index",
+            "optlableImage" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915005748.png",
+            "lottieUrl" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915022456.json",
+            "modelIos" : "JDMainPageViewController",
+            "labelColor" : "#2E2D2D",
+            "cutLabelName" : "首页",
+            "displayType" : 0,
+            "lottieMd5" : "bda3b4c642c870016b014c4d526cf954",
+            "lableImage" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915000281.png",
+            "modelAndroid" : "",
+            "bigLottieFlag" : 0
+          },
+          {
+            "lableName" : "购物车",
+            "functionId" : "cart",
+            "url" : "openappurl:dsaflkkjldasf",
+            "navigationId" : 3,
+            "lableImageMd5" : "7fba371fb402e8e5c2ae1808e26318b3",
+            "bigIconIsOpen" : "0",
+            "tabNameSelected" : "购物车",
+            "optlabelColor" : "#E2231A",
+            "useType" : -1,
+            "optlableImageMd5" : "05859b282c2af282a3f0b4dacd52600b",
+            "clickEventId" : "Cart",
+            "optlableImage" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915314277.png",
+            "lottieUrl" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915323997.json",
+            "modelIos" : "SCShopCartViewController",
+            "angleSwitch" : 1,
+            "labelColor" : "#2E2D2D",
+            "cutLabelName" : "购物车",
+            "displayType" : 0,
+            "lottieMd5" : "dbcf3ed462127d5230aa2ac8e33b491b",
+            "lableImage" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915310919.png",
+            "modelAndroid" : "",
+            "bigLottieFlag" : 0
+          },
+          {
+            "lableName" : "我的",
+            "functionId" : "home",
+            "url" : "openappurl:dsaflkkjldasf",
+            "navigationId" : 4,
+            "lableImageMd5" : "6b3f82dae360997af1b3b0286f0a2d13",
+            "bigIconIsOpen" : "0",
+            "tabNameSelected" : "我的",
+            "optlabelColor" : "#E2231A",
+            "useType" : -1,
+            "optlableImageMd5" : "fa3357e03ada7b1be6f7a21ce7b91002",
+            "clickEventId" : "Home",
+            "optlableImage" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915340710.png",
+            "lottieUrl" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915350240.json",
+            "modelIos" : "MyJdHomeViewController",
+            "labelColor" : "#2E2D2D",
+            "cutLabelName" : "我的",
+            "displayType" : 0,
+            "lottieMd5" : "ab67db802a282059455b6e0c0846870c",
+            "lableImage" : "https:\/\/storage.360buyimg.com\/mobileskin\/2791644915336267.png",
+            "modelAndroid" : "",
+            "bigLottieFlag" : 0
+          }
+        ]
+      };
+obj.result.paramValues= "Index_New_Discover_Cart_Home"
+obj.result.navigationOrder="index,cart,home"
+$done({body: JSON.stringify(obj)});
